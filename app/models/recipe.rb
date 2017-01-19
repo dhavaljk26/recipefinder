0dff6322d10e7031873eade6f5160baf6eb9f8fd
Dhaval Khandla <dhavaljkhandla26@gmail.com>
2017-01-19 23:56:30 +0530

    Second commit

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,8 @@
 class Recipe
 	include HTTParty
 
-    key_value = ENV['FOOD2FORK_KEY']
-    hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
-    base_uri "http://#{hostport}/api"
-    default_params key: "#{key_value}"
+    base_uri "http://www.food2fork.com/api"
+    default_params key: "f9f75f0383361c95a41bf2995dbefc04"
     format :json
 
   def self.for term
